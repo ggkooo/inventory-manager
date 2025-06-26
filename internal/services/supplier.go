966f0a61c7fc9c0015fb53a0f8a89811c612cc57
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2025-06-26 11:07:46 -0300

    feat: created Disponibility interface and SupplierService, implementing two interfaces

--- internal/services/supplier.go
+++ internal/services/supplier.go
@@ -2,8 +2,17 @@ package services
 
 import "fmt"
 
-type SupplierService interface {
+type InfoService interface {
 	GetInfo() string
+}
+
+type Disponibility interface {
+	VerifyDisponibility(requestedQuantity int, avaliableQuantity int) bool
+}
+
+type SupplierService interface {
+	InfoService
+	Disponibility
 }
 
 type Supplier struct {
@@ -14,4 +23,8 @@ type Supplier struct {
 
 func (s *Supplier) GetInfo() string {
 	return fmt.Sprintf("CNPJ: %s | Contact: %s | City: %s", s.CNPJ, s.Contact, s.City)
+}
+
+func (s *Supplier) VerifyDisponibility(requestedQuantity int, avaliableQuantity int) bool {
+	return requestedQuantity <= avaliableQuantity
 }
